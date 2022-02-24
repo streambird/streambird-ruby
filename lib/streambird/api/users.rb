d4785dee101993ddbe9fbc9c23633c6ccb5de13f
Timothy Chung <timchunght@gmail.com>
2022-02-23 23:44:19 -0500

    update users

--- lib/streambird/api/users.rb
+++ lib/streambird/api/users.rb
@@ -22,6 +22,25 @@ class Streambird
         return json_body
       end
 
+      def update(user_id:, 
+        first_name: nil,
+        middle_name: nil,
+        last_name: nil,
+        emails: [],
+        phone_numbers: [],
+      )
+        req = {}
+        req['first_name'] = first_name if !first_name.nil?
+        req['middle_name'] = middle_name if !middle_name.nil?
+        req['last_name'] = last_name if !last_name.nil?
+        req['emails'] = to_emails_req(emails)
+        req['phone_numbers'] = to_phone_numbers_req(phone_numbers)
+
+        response = client.put("auth/users/#{user_id}/update", req)
+        json_body = JSON.parse(response.body, symbolize_names: true)
+        return json_body
+      end
+
       def get(user_id:)
         response = client.get("auth/users/#{user_id}")
         json_body = JSON.parse(response.body, symbolize_names: true)
@@ -32,6 +51,34 @@ class Streambird
         response = client.delete("auth/users/#{user_id}/delete")
         json_body = JSON.parse(response.body, symbolize_names: true)
         return json_body
+      end
+
+      def delete_email(email_id:)
+        response = client.delete("auth/users/emails/#{email_id}/delete")
+        json_body = JSON.parse(response.body, symbolize_names: true)
+        return json_body
+      end
+
+      def delete_phone_number(phone_number_id:)
+        response = client.delete("auth/users/phone_numbers/#{phone_number_id}/delete")
+        json_body = JSON.parse(response.body, symbolize_names: true)
+        return json_body
+      end
+
+      def to_emails_req(emails)
+        emails_req = []
+        emails.each do |email|
+          emails_req << { email: email }
+        end
+        emails_req
+      end
+
+      def to_phone_numbers_req(phone_numbers)
+        phone_numbers_req = []
+        phone_numbers.each do |phone_number|
+          phone_numbers_req << { phone_number: phone_number }
+        end
+        phone_numbers_req
       end
     end
   end
